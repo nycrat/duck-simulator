43061f7fba1784904d7378ca5d7df7ae221c2b07
Eric Xiao <ericxiao0000@gmail.com>
2025-10-11 16:28:01 -0700

    refactor: rename client/server -> frontend/backend

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cd client; npm run dev -- --host --port 4420 &
-cd ../server; cargo run &
+cd frontend; npm run dev -- --host --port 4420 &
+cd ../backend; cargo run &
 cd ../admin; npx serve -l 4422 &
 
 wait
